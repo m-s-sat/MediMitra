37b587eef889278d7a5c4f5dbd57c19933f39f85
legit-os <himanshu2005sanjay@gmail.com>
2025-08-24 19:00:15 +0530

    final vector db set

--- bot.py
+++ bot.py
@@ -263,50 +263,50 @@ graph = builder.compile(checkpointer=checkpointer)
 # %%
 # graph
 
-# %%
-from langchain_core.messages import HumanMessage
-dynamic_sys= f"""{get_current_datetime_response()},
-    Location of the user=> lat=16.27939453125 & lon=80.58837890625 \n"""
-static_sys = """You are a healthcare assistant deployed on a website named "MediMitra".
-Your role is:
-1. Assist the users with there health related issues, for this you can also access a database to get some information
-   on diseases to help the user properly.
-2. Talk to the user like a professional but in a soft and cheering tone since the user is ill and he needs support.
-3. If the user wants you have to book the user's appointment with the doctor.
-4. "You must call only one tool at a time"
+# # %%
+# from langchain_core.messages import HumanMessage
+# dynamic_sys= f"""{get_current_datetime_response()},
+#     Location of the user=> lat=16.27939453125 & lon=80.58837890625 \n"""
+# static_sys = """You are a healthcare assistant deployed on a website named "MediMitra".
+# Your role is:
+# 1. Assist the users with there health related issues, for this you can also access a database to get some information
+#    on diseases to help the user properly.
+# 2. Talk to the user like a professional but in a soft and cheering tone since the user is ill and he needs support.
+# 3. If the user wants you have to book the user's appointment with the doctor.
+# 4. "You must call only one tool at a time"
 
-While booking user's appointment with a doctor follow this type of thinking:
-User input: Tell me about the doctors available in my area.
-Assistant: Calls a tool like api_retriver to get the Hospitals.
-Tool: Hospitals list near the user.
-Assistant: Calls a tool to get the doctors from the hospitals.
-Tools: Gives the doctors available
-Assistant: Tell the user about the doctors and there time slots and asks the user for which doctor to book and confirm
-about booking.
-User: Says to book some of the doctors
-Assistant: Calls a tool to book appointment of the doctor.
+# While booking user's appointment with a doctor follow this type of thinking:
+# User input: Tell me about the doctors available in my area.
+# Assistant: Calls a tool like api_retriver to get the Hospitals.
+# Tool: Hospitals list near the user.
+# Assistant: Calls a tool to get the doctors from the hospitals.
+# Tools: Gives the doctors available
+# Assistant: Tell the user about the doctors and there time slots and asks the user for which doctor to book and confirm
+# about booking.
+# User: Says to book some of the doctors
+# Assistant: Calls a tool to book appointment of the doctor.
 
-If you want to know about some type of disease or symptom related data use the disease info tool and also web search
+# If you want to know about some type of disease or symptom related data use the disease info tool and also web search
 
-"""
-# %%
-user = input("You: ")
-while user.lower() not in ["q", "quit", "exit"]:
-    llm_input = HumanMessage(content=user)
-    state = chat(messages=[llm_input],
-                 static_system=static_sys,
-                 dynamic_system=dynamic_sys,
-                 summary=""
-                 )
-    print("AI: ")
-    for chunk, meta in graph.stream(input=state,
-                                config={"configurable": {"thread_id": "6895e18fde8c65ee059b79ed"}},
-                                stream_mode="messages"
-                                ):
-        if chunk.content and meta["langgraph_node"] == "chat_node":
-            print(chunk.content,flush=True,sep="||")
-    print("\n")
-    user = input("You: ")
+# """
+# # %%
+# user = input("You: ")
+# while user.lower() not in ["q", "quit", "exit"]:
+#     llm_input = HumanMessage(content=user)
+#     state = chat(messages=[llm_input],
+#                  static_system=static_sys,
+#                  dynamic_system=dynamic_sys,
+#                  summary=""
+#                  )
+#     print("AI: ")
+#     for chunk, meta in graph.stream(input=state,
+#                                 config={"configurable": {"thread_id": "6895e18fde8c65ee059b79ed"}},
+#                                 stream_mode="messages"
+#                                 ):
+#         if chunk.content and meta["langgraph_node"] == "chat_node":
+#             print(chunk.content,flush=True,sep="||")
+#     print("\n")
+#     user = input("You: ")
 
 # # %%
 # config={"configurable": {"thread_id": "final_test"}}
