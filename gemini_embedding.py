623a5ad4936b3cea993e5b8253989d993eb2587f
legit-os <himanshu2005sanjay@gmail.com>
2025-08-24 19:08:37 +0530

    final embedding tool set

--- gemini_embedding.py
+++ gemini_embedding.py
@@ -1,5 +1,6 @@
 # %%
 from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
 from pymongo import MongoClient
 from langchain_mongodb import MongoDBAtlasVectorSearch
 import os
@@ -7,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 # %%
 embedding_model = GoogleGenerativeAIEmbeddings(model="gemini-embedding-001")
-
+llm_filter = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
 # %%
 MONGODB_ATLAS_CLUSTER_URI = os.environ["MONGOURI"]
@@ -33,7 +34,15 @@ retriver = vector_store.as_retriever(
 def disease_data_search_from_database(query: str):
     results = retriver.invoke(query)
     out = "\n".join(result.page_content for result in results)
-    return out
+    model_in = f"""Use the given data to answer the user's query:
+    Your output should be to the point and you should give every important thing related to user query. 
+    Your output must be under 500 words.
+    
+    User's query:{query}
+    
+    """
+    response = llm_filter.invoke(model_in)
+    return response.content
 
     
     
