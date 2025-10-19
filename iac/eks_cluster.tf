e16b066564dc993cb37a4b8473819cefff7fd41b
m-s-sat <mrinalsatyarthi2005@gmail.com>
2025-10-19 12:58:03 +0530

    terraform version update

--- iac/eks_cluster.tf
+++ iac/eks_cluster.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 21.0"
 
   name               = local.cluster_name
-  kubernetes_version = "1.33"
+  kubernetes_version = "1.30"
 
 
   vpc_id                 = module.vpc.vpc_id
@@ -13,14 +13,16 @@ module "eks" {
   eks_managed_node_groups = {
     one = {
       name           = "node-group-1"
-      instance_types = ["t3.small"]
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t2.medium"]
       min_size       = 1
       max_size       = 2
       desired_size   = 1
     }
     two = {
       name           = "node-group-2"
-      instance_types = ["t3.small"]
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t2.medium"]
       min_size       = 1
       max_size       = 2
       desired_size   = 1
