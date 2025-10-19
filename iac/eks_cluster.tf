cc9dbd710a591e88ac4d5b19f76b4bab2c72fdd2
m-s-sat <mrinalsatyarthi2005@gmail.com>
2025-10-19 10:57:18 +0530

    some changes in config of the eks

--- iac/eks_cluster.tf
+++ iac/eks_cluster.tf
@@ -13,17 +13,19 @@ module "eks" {
   eks_managed_node_groups = {
     one = {
       name           = "node-group-1"
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.small"]
       min_size       = 1
-      max_size       = 3
-      desired_size   = 2
+      max_size       = 2
+      desired_size   = 1
+      ami_type       = "AL2_x86_64"
     }
     two = {
       name           = "node-group-2"
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.small"]
       min_size       = 1
-      max_size       = 3
-      desired_size   = 2
+      max_size       = 2
+      desired_size   = 1
+      ami_type       = "AL2_x86_64"
     }
   }
 
