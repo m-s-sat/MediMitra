c5a91a1195285da437665fc7a34266155e20cb81
m-s-sat <mrinalsatyarthi2005@gmail.com>
2025-10-21 17:21:49 +0530

    test workflow

--- iac/eks_cluster.tf
+++ iac/eks_cluster.tf
@@ -14,7 +14,7 @@ module "eks" {
     one = {
       name           = "node-group-1"
       instance_types = ["t3.medium"]
-      ami_type       = "AL2_x86_64"
+      ami_type       = "AL2023_x86_64_STANDARD"
       min_size       = 1
       max_size       = 2
       desired_size   = 1
@@ -22,7 +22,7 @@ module "eks" {
     two = {
       name           = "node-group-2"
       instance_types = ["t3.medium"]
-      ami_type       = "AL2_x86_64"
+      ami_type       = "AL2023_x86_64_STANDARD"
       min_size       = 1
       max_size       = 2
       desired_size   = 1
